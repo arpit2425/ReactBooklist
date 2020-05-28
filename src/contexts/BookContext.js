3b4dd210f196ce58bbe3a4edc5864d4d1578940d
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-28 14:26:46 +0530

    Book Form

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,18 +1,19 @@
 import React, { useState, createContext } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 export const BookContext = createContext()
 function BookContextProvider (props) {
   const [books, setBooks] = useState([
     { title: 'name of the wind', author: 'patrick rothfuss', id: 1 },
     { title: 'the final empire', author: 'brandon sanderson', id: 2 }
   ])
-  const addBooks = (title, author) => {
-    setBooks([...books, { title, author, id: '4' }])
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: uuidv4() }])
   }
   const removeBook = id => {
     setBooks(books.filter(book => book.id !== id))
   }
   return (
-    <BookContext.Provider value={{ books, addBooks, removeBook }}>
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
       {props.children}
     </BookContext.Provider>
   )
